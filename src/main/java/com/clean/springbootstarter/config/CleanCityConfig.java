f06c7dccf8ecb44d69daa38014fb576abe4f8336
infyboys2020 <67818802+infyboys2020@users.noreply.github.com>
2021-05-29 12:58:45 +0530

    Update CleanCityConfig.java

--- src/main/java/com/clean/springbootstarter/config/CleanCityConfig.java
+++ src/main/java/com/clean/springbootstarter/config/CleanCityConfig.java
@@ -19,7 +19,7 @@ public class CleanCityConfig extends WebSecurityConfigurerAdapter{
 	        .antMatchers("/user/**").permitAll()
 	        .antMatchers("/admin/**").hasRole("ADMIN")
 	        .anyRequest().authenticated().and().formLogin().
-	        defaultSuccessUrl("/admin/reportBoard", true)
+	        defaultSuccessUrl("/admin/fetch", true)
 	        .permitAll()
 	        .and().logout()
 	        .permitAll();
